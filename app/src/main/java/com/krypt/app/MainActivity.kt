2c2e25e6abfb9e6e4cb7467e9421f982f29df759
Rahul Kumar <rahulkumar.git123@gmail.com>
2026-02-28 19:07:15 +0530

    Add import for nativeCanvas in MainActivity.kt

--- app/src/main/java/com/krypt/app/MainActivity.kt
+++ app/src/main/java/com/krypt/app/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
